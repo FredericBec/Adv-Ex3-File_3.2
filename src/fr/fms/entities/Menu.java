236b5c1432772081635bde86bc3573bbdfe77a10
Frederic Bec <bec.frederic90@outlook.fr>
2024-01-26 15:47:08 +0100

    Add display list and choice method

--- src/fr/fms/entities/Menu.java
+++ src/fr/fms/entities/Menu.java
@@ -1,6 +1,7 @@
 package fr.fms.entities;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Menu {
 
@@ -12,6 +13,17 @@ public class Menu {
 		this.name = name;
 		this.order = order;
 		this.menuItemList = menuItemList;
+	}
+	
+	public Menu(String name, ArrayList<String> menuItemList) {
+		this.name = name;
+		this.menuItemList = menuItemList;
+	}
+	
+	public Menu() {
+		this.name = "uknown";
+		this.order = null;
+		this.menuItemList = null;
 	}
 
 	public String getName() {
@@ -38,12 +50,27 @@ public class Menu {
 		this.menuItemList = menuItemList;
 	}
 
-	public void composeMenu(ArrayList<String> menuItemList) {
-		setMenuItemList(menuItemList);
+	public String composeMenu(Scanner scan, ArrayList<String> menuItemList, String menuItem) {
+		int maxChoice = this.displayMenuItemList(menuItemList);
+		System.out.println("Que souhaitez vous comme " + menuItem.toLowerCase() + ":");
+		int starterChoice = 0;
+		while(starterChoice < 1 || starterChoice > maxChoice) {
+			
+			starterChoice = scan.nextInt();
+		}
+		return menuItemList.get(starterChoice);
+	}
+	
+	public int displayMenuItemList(ArrayList<String> menuItemList) {
+		for(int i = 0; i < menuItemList.size(); i++) {
+			System.out.print("[" + i + " - " + menuItemList.get(i) + "]");
+		}
+		System.out.println();
+		return menuItemList.size();
 	}
 
 	@Override
 	public String toString() {
-		return "Menu [name=" + name + "]";
+		return "Menu :" + name + " " + menuItemList;
 	}
 }
